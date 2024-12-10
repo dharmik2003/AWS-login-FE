8ea6204005fbc2dc452784de8dc15d37322a4c9e
Dharmik Kachhadiya <99231778+dharmik2003@users.noreply.github.com>
2024-12-10 17:03:59 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import Login from "./components/Login";
+
 export default function Home() {
   return (
-    <div>HOme</div>
+    <div className="w-screen h-screen ">
+      <Login />
+    </div>
   );
 }
