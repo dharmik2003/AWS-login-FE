a2c21069aafb984ad1bbbf20d206972a6b388e56
Dharmik Kachhadiya <99231778+dharmik2003@users.noreply.github.com>
2024-12-23 13:32:10 +0530

    Update Login.tsx

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -46,7 +46,7 @@ const Login: React.FC = () => {
 
     try {
       // Make the API call
-      const response = await fetch("https://codedharmik/login", {
+      const response = await fetch("https://codedharmik/api/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
