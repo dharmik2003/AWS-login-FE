6a884bf6931e49a6123ada8fa0ae3365abe2b553
Dharmik Kachhadiya <99231778+dharmik2003@users.noreply.github.com>
2024-12-23 13:24:08 +0530

    Update Dashboard.tsx

--- app/components/Dashboard.tsx
+++ app/components/Dashboard.tsx
@@ -17,7 +17,7 @@ export default function Dashboard() {
 
   const fetchUserList = async () => {
     try {
-      const response = await fetch("http://codedharmik/users", {
+      const response = await fetch("https://codedharmik/users", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
